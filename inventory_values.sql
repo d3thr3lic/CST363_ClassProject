fde68fd7ba0d6a76d46b28f66629f52e12af5122
misternomar <rlucindo@csumb.edu>
2019-10-10 20:34:27 -0700

    Create inventory_values.sql

---
+++ inventory_values.sql
@@ -0,0 +1,11 @@
+INSERT INTO inventory VALUES
+('MNR001', 'Variety Pack No Grain(Lamb, Chicken, Beef, Salmon and Turkey) 50 cans/box', 100, 459.99),
+('MNR002', 'No Grain Lamb Natural Mix 50 cans/box', 100, 459.99),
+('MNR003', 'No Grain Beef Natural Mix 50 cans/box', 100, 459.99),
+('MNR004', 'No Grain Chicken Natural Mix 50 cans/box', 100, 459.99),
+('MNR005', 'No Grain Salmon Natural Mix 50 cans/box', 100, 459.99),
+('MNR006', 'No Grain Turkey Natural Mix 50 cans/box', 100, 459.99),
+('MNR007', 'No Grain Duck Natural Mix 50 cans/box', 100, 459.99),
+('MNR008', 'All Natural Beef Chews 10 sticks/pouch, 50/box', 100, 499.99),
+('MNR009', 'All Natural Chicken Chews 10 sticks/pouch, 50/box', 100, 499.99), 
+('MNR010', 'All Natural Duck Chews 10 sticks/pouch, 50/box', 100, 499.99)
